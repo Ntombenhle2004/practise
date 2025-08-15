9d9e110381239843dc9ce0e018d3c7db1eb9ad9a
ntorh <ngcobontombenhle434@gmail.com>
2025-08-15 11:57:40 +0200

    learning loops

---
+++ App.js
@@ -0,0 +1,47 @@
+let sentence = "Hello world, I am Ntorh";
+//  console.log(sentence[3])
+let i = 0;
+for (let i = 0; i < 6; i++){
+    // console.log(sentence[i]);
+}
+
+
+
+// let listBook = ["dune", "fall", "game"];
+
+// for (let 1 = 0; i < 6; ) {
+//   console.log(listBook[i][0]);
+//   
+// }
+
+let listBook = ["dune", "fall", "game"];
+
+for (let i = 0; i < listBook.length; i++) {
+//   console.log(listBook[i][0]);
+}
+
+
+for (k of sentence){
+    //  console.log(k);
+}
+
+for (k of listBook) {
+//   console.log(k);
+}
+
+let marks = {
+    ntoh: 50,
+    Alice: 85,
+    Bob: 92,
+    sbonelo: 85,
+    Charlie: 78
+};
+for (let x in marks) {
+//   console.log(x + " : " + marks["Alice"]);
+}
+
+while(i<6){
+   console.log(k); 
+}
+
+
---
+++ movies.js
@@ -0,0 +1,10 @@
+const movies = [
+  {
+    title: "Fall",
+    director: "Sneh",
+    year: 2020,
+    genre: "Action",
+    rating: "PG-13",
+    actors: ["Ntombi", "Thabiso", "Mello"],
+  },
+];
